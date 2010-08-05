a9b43e6bb8458dfa4742debb8f9d2c7057579fa1
Wilfred <me@wilfred.me.uk>
2010-08-05 23:00:30 +0100

    moving episode text fixes to a separate method, making varsovia vento fixing more robust

--- podcasts.php
+++ podcasts.php
@@ -51,38 +51,15 @@ function print_episode($episode) {
   $description = $episode->get_description();
   $mp3 = $episode->get_enclosure()->get_link();
   $date = translate_date($episode->get_local_date('%A %e %B %Y'));
+  $feed_description = $episode->get_feed()->get_description();
 
-  if (strlen($description) == 0 or $description == $title) {
-    $formatted_description = 'Neniu priskribo.';    
-  } else if ($name == 'Varsovia Vento Elsendoj') {
-    // remove junk from varsovia vento's description
-    // keep only the middle paragraph
-    $paragraphs = explode("\n\n", $description);
-
-    $sentences = explode(".", $paragraphs[0]);
-    $intro_sentence = 'En ĉi tiu epizodo: '.$sentences[0].'.';
-    $formatted_description = $intro_sentence.'</p><p>'.$paragraphs[1];
-  } else if ($name == 'Radio Verda') {
-    // add trailing full stop
-    $formatted_description = 'En ĉi tiu epizodo: '.$description.'.';
-  } else {
-    $formatted_description = 'En ĉi tiu epizodo: '.$description;
-  }
-
-  if ($episode->get_feed()->get_description() == 'Radio Vatikana') {
-    // sadly radio vatikana doesn't say its name in its feed
-    $name = 'Radio Vatikana';
-  } else if ($name == 'Album Junula Radio Internacia from LaPingvino') {
-    //JRI is from a RSS feed on ipernity, so rename it
-    $name = 'Junula Radio Internacia';
-    $formatted_description = 'Neniu priskribo.';
-  }
+  polish_episode($name, $title, $description, $mp3, $date, $feed_description);
 
   $prepared_div = <<<EOT
 <div class="programo">
 <h3>$name: $title</h3>
 
-<p>$formatted_description</p>
+<p>$description</p>
 
 <p><em>$date</em></p>
 EOT;
@@ -101,6 +78,52 @@ EOT;
   }
 
   print $prepared_div . $end_div;
+}
+
+function polish_episode(&$name, &$title, &$description, &$mp3,
+			&$date, $feed_description) {
+  //make all text relating to episodes nicer
+
+  // fix dodgy names
+  if ($feed_description == 'Radio Vatikana') {
+    // sadly radio vatikana doesn't say its name in its feed
+    $name = 'Radio Vatikana';
+  } else if ($name == 'Album Junula Radio Internacia from LaPingvino') {
+    //JRI is from a RSS feed on ipernity, so rename it
+    $name = 'Junula Radio Internacia';
+  }
+
+  //polish descriptions
+  if (strlen($description) == 0 or $description == $title or 
+      $name == 'Junula Radio Internacia') {
+    // no description if no episode title or same as the station name
+    $description = 'Neniu priskribo.';    
+  } else if ($name == 'Varsovia Vento Elsendoj') {
+    // remove junk from varsovia vento's description
+    // junk takes the rough form 'Download audio file (100418vve057a.mp3) \n Elŝutu   podkaston
+    $description = preg_replace("/Download audio file .*? podkaston?/is", "", $description);
+    // and sometimes the junk is truncated :-/
+    $description = preg_replace("/Download audio file/i", "", $description);
+    $paragraphs = explode("\n\n", $description);
+
+    // now convert line breaks into html paragraphs
+    $start = True;
+    foreach ($paragraphs as $paragraph) {
+      if ($start) {
+	$description = 'En ĉi tiu epizodo: '.$paragraph;
+	$start = False;
+      } else {
+	$description = $description."</p>\n\n<p>".$paragraph;
+      }
+    }
+  } else if ($name == 'Radio Verda') {
+    // add trailing full stop
+    $description = 'En ĉi tiu epizodo: '.$description.'.';
+  } else {
+    // default
+    $description = 'En ĉi tiu epizodo: '.$description;
+  }
+
 }
 
 function translate_date($english_date) {
