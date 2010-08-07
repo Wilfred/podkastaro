48cc1833ba68a07ebfab3d091abbfaccce42e9ef
Wilfred <me@wilfred.me.uk>
2010-08-07 11:42:55 +0100

    adding new about page that conforms to page naming scheme

---
+++ pri-podkastaro.php
@@ -0,0 +1,43 @@
+<?php
+include 'common.php';
+print_header();
+print_sidebar('Pri Podkastaro');
+print_begin_main();
+?>
+
+<h3>Kio estas podkastaro?</h3>
+
+<p>Podkastaro estas simpla retejo, kie oni povas trovi ĉiujn
+esperantajn podkastojn.</p>
+
+<h3>Kial vi ne havas arĥivojn?</h3>
+
+<p>Mi volis krei Podkastaron rapide kaj facile, tial ĝi ne ofertas
+  arĥivojn hodiaŭ. Nuntempe mi verkas aliajn esperantajn projektojn,
+  sed mi volas aldoni arĥivojn en la estonteco.</p>
+
+<h3>Ĉu mi povas uzi vian kodon?</h3>
+
+<p>Jes ja! Vi povas trovi ĝin
+ĉe <a href="http://github.com/Wilfred/podkastaro">GitHub</a>. Bedaŭrinde
+mi skribis la PHP-an kondon en la angla, do kontaktu min se vi ne
+komprenas ĝin.</p>
+
+<h3>Kiu estas vi?</h3>
+
+<p>Saluton! Mi estas juna brita esperantisto kiu volis siajn
+  komputikajn povojn por helpi esperantistojn. Mi
+  havas <a href="http://www.littlelinguist.org/">blogon</a>, mia
+  skajpkonto estas vilfredo_ kaj mi ofte 
+  <a href="http://twitter.com/vilfredo_">uzas tviteron</a>.</p>
+
+<p>Se oni volas kontakti min rekte, uzu la
+  formon <a href="http://lalingvisto.wordpress.com/pri-mi/">ĉi
+  tie</a>.</p>
+
+<p><em>Vilfredo</em></pm>
+
+<?php
+print_end_main();
+print_footer();
+
